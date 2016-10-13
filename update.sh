3d9c3a0050801b764581f60103f4a1aa4914fd1b
Matrix89 <matrix8910@gmail.com>
2016-10-13 13:10:28 +0200

    add bash_profile bashrc to update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,9 @@ cp ~/.config/bspwm/bspwmrc ./bspwm/
 cp ~/.config/sxhkd/sxhkdrc ./sxhkd/
 cp ~/.config/bar/bar.sh ./bar/
 cp ~/.config/Xresources ./
-cp ~/.vimrc vimrc
+cp ~/.vimrc ./vimrc
 cp ~/.config/compton.conf ./
+cp ~/.bash_profile ./bash_profile
+cp ~/.bashrc ./bashrc
 
 git status
